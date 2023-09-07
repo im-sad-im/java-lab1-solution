25e62d819a7fa3c01d5a2e81d94071e98232268b
Sadim Mali <sadimmalakar77@gmail.com>
2023-09-07 16:12:19 +0545

    Add .gitignore

--- CommandLine.java
+++
@@ -1,9 +0,0 @@
-public class CommandLine {
-    public static void main(String[] args) {
-       int firstNum = 10;
-       int secondNum = 20;
-       int sum = 0;
-       sum = firstNum + secondNum;
-       System.out.println("Sum : " + sum);
-}
-}
